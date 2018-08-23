790ce90eca8b00c8b4ea597422c58bf9f34bbd20
fengruiliu <42656891+fengruiliu@users.noreply.github.com>
2018-08-23 17:05:18 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
+/**
+*Author: Chaofan Song and Fengrui liu 
+*Date: 2018/08/23
+*
+*A sample hello word program in C
+*
+*/
+#include <stdlib.h>
 #include <stdio.h>
-int main()
-{
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+int main(int argc, char **argv) {
+   
+   printf("Chaofan Song and Fengrui Liu Computer Science\n");
+   
    return 0;
 }
